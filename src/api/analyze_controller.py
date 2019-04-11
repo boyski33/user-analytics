e473695509f757ed0f6384953a4c1ba14f2f7a15
Boyan Kushlev <takarabukara@gmail.com>
2019-04-11 23:24:30 +0300

    add pymongo and connect with atlas

--- src/api/analyze_controller.py
+++ src/api/analyze_controller.py
@@ -1,6 +1,14 @@
 from src.run import app
+from src.core.idk_service import Service
+from flask import Response
+import json
+
+service = Service()
 
 
 @app.route("/")
 def hello():
-    return "working"
+    all_surveys = service.get_all_surveys()
+    print(all_surveys)
+
+    return Response(all_surveys, mimetype='application/json')
