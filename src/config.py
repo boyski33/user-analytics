bf91bb72dfd4f81c12f3d08f8b7813cb31468c13
Boyan Kushlev <takarabukara@gmail.com>
2019-05-08 01:19:33 +0300

    add workaround deploy

--- src/config.py
+++ src/config.py
@@ -1,7 +1,6 @@
 config = {
     "service_name": "HIPPO-USER-ANALYTICS",
     "service_port": 8855,
-    "eureka_server_url": "http://localhost:8761/eureka",
     "mongo_connection": "mongodb+srv://admin:admin@hippo-cluster-gya0k.mongodb.net/hippo-survey-db?retryWrites=true",
     "class_columns": ["age", "gender"],
     "supported_question_types": ["textbox", "dropdown", "radio", "yesno", "range"]
--- src/run.py
+++ src/run.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import os
-
-import py_eureka_client.eureka_client as eureka_client
 from flask import Flask
 
 from config import config
@@ -14,19 +11,6 @@ from api.analytics_controller import *
 
 
 def run_app():
-
-    eureka_url = os.environ.get('EUREKA', config["eureka_server_url"])
-    eureka_client.init(eureka_server=eureka_url,
-                       app_name=config["service_name"],
-                       instance_port=config["service_port"])
-
-    try:
-        res = eureka_client.do_service("HIPPO-CORE-SURVEY", "/surveys")
-        print("result: " + res)
-        print("{}SUCCESSFULLY REGISTERED ON EUREKA SERVER{}".format("\033[92m", "\033[0m"))
-    except Exception as e:
-        print(e)
-
     app.run(port=config["service_port"])
 
 
