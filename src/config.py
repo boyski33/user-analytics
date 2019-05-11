6d144084f45e30f3cd61bebb9b6e60994c96d7a8
Boyan Kushlev <takarabukara@gmail.com>
2019-05-11 13:34:40 +0300

    change port

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 config = {
     "service_name": "HIPPO-USER-ANALYTICS",
-    "service_port": 8855,
+    "service_port": 80,
     "mongo_connection": "mongodb+srv://admin:admin@hippo-cluster-gya0k.mongodb.net/hippo-survey-db?retryWrites=true",
     "class_columns": ["age", "gender"],
     "supported_question_types": ["textbox", "dropdown", "radio", "yesno", "range"]
